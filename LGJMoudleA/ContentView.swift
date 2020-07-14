e1b15ecff990f4752450768f18710de8dd43bd80
ligongjiao <381216970@qq.com>
2020-07-14 14:00:40 +0800

    init

---
+++ LGJMoudleA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LGJMoudleA
+//
+//  Created by Alex on 2020/7/14.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+inhibit_all_warnings!
+
+target 'LGJMoudleA' do
+end
