5ef850a94c147b678d1916c3975bae195e080dd5
lipopli <381216970@qq.com>
2020-07-24 15:56:13 +0800

    Add files via upload

---
+++ LGJMoudleA/Tool/LGJHudHelper.swift
@@ -0,0 +1,17 @@
+//
+//  LGJHudHelper.swift
+//  LGJMoudleA
+//
+//  Created by Alex on 2020/7/23.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class LGJHudHelper: NSObject {
+
+    static func showHud(info : String) {
+        
+        print("showHud")
+    }
+}
